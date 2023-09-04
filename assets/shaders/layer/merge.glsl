db7e9948e1c6ac7565161b036a5e8eef5126a1d1
GiantLuigi4 <49770992+GiantLuigi4@users.noreply.github.com>
2023-09-03 23:00:12 -0400

    at some point I accidentally removed compute shader support from merge.glsl

--- assets/shaders/layer/merge.glsl
+++ assets/shaders/layer/merge.glsl
@@ -3,15 +3,21 @@ precision highp float;
 precision highp int;
 precision highp sampler2D;
 
+#ifdef COMPUTE
+    layout (local_size_x = 32, local_size_y = 32, local_size_z = 1) in;
+    layout(rgba16, binding = 0) uniform image2D Output;
+    layout(binding = 1) uniform sampler2D Tex0;
+    layout(binding = 2) uniform sampler2D Tex1;
+#else
     in vec2 TexCoord;
     out vec4 color;
-
-uniform sampler2D Tex0; // uniform-Tex0
-uniform sampler2D Tex1; // uniform-Tex1
+    uniform sampler2D Tex0;
+    uniform sampler2D Tex1;
+#endif
 
 uniform float Alpha;
 uniform int Function;
-uniform vec2 Resolution; // uniform-Resolution
+uniform vec2 Resolution;
 
 //@formatter:off
 vec4 blend(vec4 dst, vec4 src) {
@@ -47,13 +53,17 @@ vec4 blend(vec4 dst, vec4 src) {
 //@formatter:on
 
 void main() {
-    ivec2 coord = ivec2(TexCoord * Resolution);
+    #ifdef COMPUTE
+        ivec2 coord = ivec2(gl_GlobalInvocationID.xy);
+    #else
+        ivec2 coord = ivec2(TexCoord * Resolution);
+    #endif
 
     vec4 bottom;
-    if (Resolution == textureSize(Tex0, 0)) { bottom = texelFetch(Tex0, ivec2(coord), 0); }
+    if (Resolution == textureSize(Tex0, 0)) { bottom = texelFetch(Tex0, coord, 0); }
     else { bottom = texture2D(Tex0, coord.xy / Resolution); }
     vec4 top;
-    if (Resolution == textureSize(Tex1, 0)) { top = texelFetch(Tex1, ivec2(coord), 0); }
+    if (Resolution == textureSize(Tex1, 0)) { top = texelFetch(Tex1, coord, 0); }
     else { top = texture2D(Tex1, coord.xy / Resolution); }
     top.a *= Alpha;
 
